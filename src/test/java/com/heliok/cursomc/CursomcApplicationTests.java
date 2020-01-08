2909ea9b6cabc53cd3dc7abd32033c27fcee6b63
Heliok <z2hisak@gmail.com>
2020-01-08 13:36:43 -0300

    Projeto criado

---
+++ src/test/java/com/heliok/cursomc/CursomcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heliok.cursomc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursomcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
