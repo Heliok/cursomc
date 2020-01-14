48dc0a181429fad221bd4522c180ec48ca416081
Heliok <z2hisak@gmail.com>
2020-01-14 15:54:46 -0300

    Estado e Cidade

---
+++ src/main/java/com/heliok/cursomc/repositories/CidadeRepository.java
@@ -0,0 +1,13 @@
+package com.heliok.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.heliok.cursomc.domain.Categoria;
+import com.heliok.cursomc.domain.Cidade;
+
+@Repository
+public interface CidadeRepository extends JpaRepository<Cidade, Integer> {
+
+}
+
---
+++ src/main/java/com/heliok/cursomc/repositories/EstadoRepository.java
@@ -0,0 +1,12 @@
+package com.heliok.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.heliok.cursomc.domain.Estado;
+
+@Repository
+public interface EstadoRepository extends JpaRepository<Estado, Integer> {
+
+}
+
