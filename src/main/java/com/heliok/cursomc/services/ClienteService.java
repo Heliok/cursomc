3b61c9c773d4c271abb8ae5bc869cf98c6953dc1
Heliok <z2hisak@gmail.com>
2020-01-16 16:06:37 -0300

    Endpoint /clientes/{id} disponivel

---
+++ src/main/java/com/heliok/cursomc/services/ClienteService.java
@@ -0,0 +1,23 @@
+package com.heliok.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.heliok.cursomc.domain.Cliente;
+import com.heliok.cursomc.repositories.ClienteRepository;
+import com.heliok.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+
+	@Autowired
+	private ClienteRepository repo;
+
+	public Cliente find(Integer id) {
+		Optional<Cliente> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
+	}
+}
