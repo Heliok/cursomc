1bdc9e062b56d1eb7fb1575105735f9c1817751c
Heliok <z2hisak@gmail.com>
2020-01-31 17:27:14 -0300

    Validacao customizada: email nao repetido na insercao de Cliente

--- src/main/java/com/heliok/cursomc/repositories/CidadeRepository.java
+++ src/main/java/com/heliok/cursomc/repositories/CidadeRepository.java
@@ -3,7 +3,6 @@ package com.heliok.cursomc.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.heliok.cursomc.domain.Categoria;
 import com.heliok.cursomc.domain.Cidade;
 
 @Repository
--- src/main/java/com/heliok/cursomc/repositories/ClienteRepository.java
+++ src/main/java/com/heliok/cursomc/repositories/ClienteRepository.java
@@ -2,11 +2,15 @@ package com.heliok.cursomc.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.heliok.cursomc.domain.Cliente;
 
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
+	
+	@Transactional(readOnly=true)
+	Cliente findByEmail(String email);
 
 }
 
